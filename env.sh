9bd1bce30686cf29b95ff873792250e207d523eb
Martin Lehmann <martin.lehmann@accso.de>
2020-11-06 14:30:46 +0100

    Fix local path for GRAALVM_HOME

--- env.sh
+++ env.sh
@@ -1,7 +1,6 @@
 # Environment settings
 
-# export GRAALVM_HOME=/TODO/PATH/TO/GRAALVM/INSERT/HERE
-export GRAALVM_HOME=/g/graal/graalvm-ce-java11-20.2.0
+export GRAALVM_HOME=/TODO/PATH/TO/GRAALVM/INSERT/HERE
 export JAVA_HOME=${GRAALVM_HOME}
 
 export PATH=${PATH}:${JAVA_HOME}/bin:${GRAALVM_HOME}/bin:/g/bin
