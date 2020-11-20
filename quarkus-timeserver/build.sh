2ae447dd93f219bd02f0e532653935f9a4d307c4
Ruediger Grammes <ruediger.grammes@accso.de>
2020-11-20 17:05:26 +0100

    add commands for building native image with profiling (EE only)

--- quarkus-timeserver/build.sh
+++ quarkus-timeserver/build.sh
@@ -9,3 +9,9 @@ mkdir -p ${TARGET}
 
 # including the Docker image
 # ./mvnw clean package -Pnative -Dquarkus.native.container-build=true
+
+# build profiling image
+# ./mvnw clean package -Pnative -Dquarkus.native.additional-build-args=--pgo-instrument
+
+# build using profiling information (might need to add path to iprof file)
+#./mvnw clean package -Pnative -Dquarkus.native.additional-build-args="--pgo=default.iprof"
