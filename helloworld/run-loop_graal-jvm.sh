3805764757073832c55ce10b5c09e2021f08c35b
martin.lehmann@accso.de <martin.lehmann@accso.de>
2021-06-23 14:29:10 +0200

    add time to run-loop scripts

--- helloworld/run-loop_graal-jvm.sh
+++ helloworld/run-loop_graal-jvm.sh
@@ -3,7 +3,12 @@ if [ "$1" != "" ]; then MAX=$1; fi
 
 which java
 
-for ((i=1; i<=${MAX}; i++))
-do
-  java -cp target HelloWorld
-done
+function runinloop() 
+{
+	for ((i=1; i<=${MAX}; i++))
+	do
+	  java -cp target HelloWorld
+	done
+}
+
+time runinloop
