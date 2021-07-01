64d598cd9c016612d42824ec74ac204c3ef99eb3
martin.lehmann@accso.de <martin.lehmann@accso.de>
2021-07-01 21:15:21 +0200

    fixed jmeter run script

--- quarkus-timeserver/run_jmeter-loadtest.sh
+++ quarkus-timeserver/run_jmeter-loadtest.sh
@@ -1,5 +1,5 @@
 # JMETER_HOME=/TODO/PATH/TO/JMETER/INSERT/HERE
-JMETER_HOME=/opt/jmeter/apache-jmeter-5.3/bin
+JMETER_HOME=/opt/jmeter/apache-jmeter-5.3
 
 NOW=`date +%s%3N`
 
@@ -8,4 +8,4 @@ JMETER_TMPDIR=${TMPDIR}/jmeter-${NOW}
 
 mkdir -p ${JMETER_TMPDIR}
 
-bash ${JMETER_HOME}/jmeter.sh -n -t jmeter-timeserver.jmx -e -o ${JMETER_TMPDIR} -l ${JMETER_TMPDIR}/output-${NOW}.txt
+bash ${JMETER_HOME}/bin/jmeter.sh -n -t jmeter-timeserver.jmx -e -o ${JMETER_TMPDIR} -l ${JMETER_TMPDIR}/output-${NOW}.txt
