f77684a9c99d48cd9e34c37d2a50cc37327c5954
martin.lehmann@accso.de <martin.lehmann@accso.de>
2021-08-16 10:49:13 +0200

    correct Mandelbrot example so that now a jni-config.json is created which is then used to create a native image (prevents runtime errors like 'java.lang.NoSuchFieldError: java.awt.image.ColorModel.pData')

--- mandelbrot/run1_toPNGFile-java.sh
+++ mandelbrot/run1_toPNGFile-java.sh
@@ -5,11 +5,21 @@ which java
 
 # -----------------------------------------------------------------------------------------------------------------
 
+# Sizing factor (1 is default)
+
+factor=1
+if [ ! ${1} == "" ];
+then
+   factor=$1
+fi
+
+# -----------------------------------------------------------------------------------------------------------------
+
 pushd ${TARGET} >/dev/null 
 
-echo "1) runs graal in jvm mode, creating mandelbrot PNG file in /tmp"
+echo "1) runs graal in jvm mode, creating mandelbrot PNG file (factor $factor) in /tmp"
 
-time java -Djava.io.tmpdir=/tmp -cp classes MandelbrotToPNGFile
+time java -Djava.io.tmpdir=/tmp -cp classes MandelbrotToPNGFile $factor
 
 # -----------------------------------------------------------------------------------------------------------------
 
--- mandelbrot/run2_toPNGFile-native-image.sh
+++ mandelbrot/run2_toPNGFile-native-image.sh
@@ -5,10 +5,19 @@ which java
 
 # -----------------------------------------------------------------------------------------------------------------
 
+# Sizing factor (1 is default)
+
+factor=1
+if [ ! ${1} == "" ];
+then
+   factor=$1
+fi
+
+# -----------------------------------------------------------------------------------------------------------------
+
 pushd ${TARGET} >/dev/null 
 
-echo "1) runs graal in  native-image mode, creating mandelbrot PNG file in /tmp "
-echo "   shows error, dies at runtime with exception (as awt not available at runtime)"
+echo "2) runs graal in native-image mode, creating mandelbrot PNG file (factor $factor) in /tmp "
 
 time ./mandelbrotToPNGFile $factor
 
