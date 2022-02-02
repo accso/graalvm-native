187f32d5cb70de9eeb7e1f00ac805706c4f1cce5
martin.lehmann@accso.de <martin.lehmann@accso.de>
2022-02-02 16:01:02 +0100

    update GraalVM native example to GraalVM 22.0.0.2, JDK17

--- printproperties/build.sh
+++ printproperties/build.sh
@@ -15,20 +15,20 @@ pushd ${TARGET} >/dev/null
 
 # -----------------------------------------------------------------------------------------------------------------
 
-echo "1) create native-image, set a build-time property with -D"
+echo "1) create native-image, set a build-time property with -D, which has no effect"
 
 ${GRAALVM_HOME}/bin/native-image --no-fallback \
         -Dmy.buildtime.flag=true \
         -H:+PrintAnalysisCallTree -H:+ReportExceptionStackTraces \
-		PrintProperties printProperties 
+	    PrintProperties printProperties 
 
 # -----------------------------------------------------------------------------------------------------------------
 
-echo "2) create native image, same build-time property but this time initializing *all* classes at build time"
+echo "2) create native image, set same build-time property but this time initializing the class at build time"
 
 ${GRAALVM_HOME}/bin/native-image --no-fallback \
-        --initialize-at-build-time -Dmy.buildtime.flag=true \
+        --initialize-at-build-time=PrintProperties -Dmy.buildtime.flag=true \
         -H:+PrintAnalysisCallTree -H:+ReportExceptionStackTraces \
-		PrintProperties printProperties-buildtime \
+		PrintProperties printProperties-buildtime
 
 popd >/dev/null
